107c2e0560d6b239d99e15cfe051712b636d8e29
danigonz <daniel-g-j@hotmail.com>
2020-07-30 16:24:59 +0200

    add drag_and_drop appium

--- driver/typeDriver/appium/utilsAppium/utilsDriverAppium.py
+++ driver/typeDriver/appium/utilsAppium/utilsDriverAppium.py
@@ -1,44 +1,46 @@
-from appium.webdriver.webdriver import *
+from appium.webdriver import WebElement
 from appium.webdriver.common.touch_action import TouchAction
 import time
 
 
-class utilsDriverAppium:
+class UtilsDriverAppium:
 
     def __init__(self, driver):
-        self.driver: WebDriver = driver
+        self.driver = driver
 
     def sleep(self):
         time.sleep(5)
 
-    def close(self):
-        self.driver.close()
-
-    def quit(self):
-        self.driver.quit()
-
     def wait(self, seconds):
         self.driver.implicitly_wait(seconds)
 
-    def pressLongElement(self, element, milliseconds):
+    def press_long_element(self, element: WebElement, milliseconds: int):
         actions = TouchAction(self.driver)
         actions.press(element)
         actions.wait(milliseconds)
         actions.release().perform()
 
-    def pressLongPosition(self, posX, posY, milliseconds):
+    def press_long_position(self, pos_x, pos_y, milliseconds: int):
         actions = TouchAction(self.driver)
-        actions.press(x=posX, y=posY).release().perform()
-
-    def swipe(self, iniPosX, iniPposY, FinalPosX, FinalPosY, milliseconds):
-        actions = TouchAction(self.driver)
-        actions.press(x=iniPosX, y=iniPposY)
         actions.wait(milliseconds)
-        actions.move_to(x=FinalPosX, y=FinalPosY)
+        actions.press(pos_x, pos_y).release().perform()
+
+    def swipe(self, ini_pos_x, ini_pos_y, final_pos_x, final_pos_y, milliseconds: int):
+        actions = TouchAction(self.driver)
+        actions.press(ini_pos_x, ini_pos_y)
+        actions.wait(milliseconds)
+        actions.move_to(final_pos_x, final_pos_y)
         actions.release().perform()
 
-    def hideKeyboard(self):
+    def hide_keyboard(self):
         self.driver.hide_keyboard()
 
-    def getTimePhone(self):
+    def get_time_phone(self):
         self.driver.get_device_time()
+
+    def drag_and_drop(self, element: WebElement, final_pos_x, final_pos_y, milliseconds: int):
+        actions = TouchAction(self.driver)
+        actions.long_press(element)
+        actions.wait(milliseconds)
+        actions.move_to(final_pos_x, final_pos_y)
+        actions.release().perform()
