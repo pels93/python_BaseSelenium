f0ea51863b26eb2cbe0a8293082d6cdbfcc73a4c
pels93 <daniel-g-j@hotmail.com>
2020-08-03 16:24:47 +0200

    refactor

--- driver/typeDriver/appium/appium.py
+++ driver/typeDriver/appium/appium.py
@@ -1,7 +1,7 @@
 from driver.typeDriver.utilsSelectDriver.aux_config import *
 from driver.typeDriver.appium.utilsAppium.initDriverAppium import StartDriverAppium
-from driver.typeDriver.appium.utilsAppium.utilsDriverAppium import utilsDriverAppium
-from driver.typeDriver.appium.utilsAppium.UtilsMobileElements import UtilsMobileElements
+from driver.typeDriver.appium.utilsAppium.utilsDriverAppium import UtilsDriverAppium
+from driver.typeDriver.appium.utilsAppium.utilsMobileElements import UtilsMobileElements
 
 
 #   https://pypi.org/project/Appium-Python-Client/
@@ -10,5 +10,5 @@ class Appium:
     def __init__(self):
         self.platform = ReadConfig.get_mobile_platform()
         self.driver = StartDriverAppium.start_driver_platform(self.platform)
-        self.utilsDriver = utilsDriverAppium(self.driver)
+        self.utilsDriver = UtilsDriverAppium(self.driver)
         self.utilsMobileElements = UtilsMobileElements(self.driver)
