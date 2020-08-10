00d9ff69d7c103401c0c7dd4603a06ab73ae80c0
pels93 <daniel-g-j@hotmail.com>
2020-08-10 17:44:53 +0200

    update bats add  ,&echo "enableDeleteOldReport": true

--- run_mobile.bat
+++ run_mobile.bat
@@ -1,5 +1,5 @@
 @echo off
-(echo {&echo "typeDriver": "appium",&echo "mobilePlatform":"android",&echo "urlServerAppium":"http://127.0.0.1:4723/wd/hub",&echo "mobileLanguage":"ES_es",&echo "app":"test.apk",&echo "nameMobile":"Pixel_2_API_27",&echo "versionMobile":"8.1.0",&echo "adbName":"emulator-5554"&echo }) > settings.json
+(echo {&echo "typeDriver": "appium",&echo "mobilePlatform":"android",&echo "urlServerAppium":"http://127.0.0.1:4723/wd/hub",&echo "mobileLanguage":"ES_es",&echo "app":"test.apk",&echo "nameMobile":"Pixel_2_API_27",&echo "versionMobile":"8.1.0",&echo "adbName":"emulator-5554",&echo "enableDeleteOldReport": true &echo }) > settings.json
 adb kill-server
 start %ANDROID_HOME%\tools\emulator.exe -avd Pixel_2_API_27 -no-snapshot -no-snapshot-load -no-snapshot-save -memory 512 -timezone Europe/Paris -no-boot-anim -no-audio -nocache
 adb devices
