e51007d2aea8e9c700901754a26e1aa686217759
pels93 <daniel-g-j@hotmail.com>
2020-09-04 19:03:54 +0200

    fixed steps

--- features/feature/web/google.feature
+++ features/feature/web/google.feature
@@ -3,19 +3,18 @@ Feature: navegar por google
   @nasa @google
   Scenario: El navegador busca "nasa"
     Given Encender el navegador
-    When  El navegador introduce la URL "https://google.es"
-    And   Se visualiza la pagina de busqueda de google
+    When  El navegador introduce la URL "https://www.nasa.gov/"
+    And   El navegador espera "3" segundos
+    And   Comprobar que lleva a "https://www.nasa.gov/"
     And   se abre una nueva pestana
     And   se selecciona la "1" pestana
     And   El navegador introduce la URL "https://starfriend.es"
-    And   se selecciona la "1" pestana
+    And   El navegador espera "3" segundos
     And   se cierra la pestana "1"
     And   se selecciona la "0" pestana
-    And   Buscar en google por "nasa"
-    And   Seleccionar el primer resultado en google
     Then  Comprobar que lleva a "https://www.nasa.gov/"
     And imprimir logs consola
- 
+
   @google
   Scenario Outline:El navegador busca <palabra>
     Given Encender el navegador
@@ -25,5 +24,5 @@ Feature: navegar por google
     And   Seleccionar el primer resultado en google
     Then  Comprobar que lleva a <urlrevisar>
     Examples:
-    | urls                 | palabra          |  urlrevisar                   |
-    | "https://google.es"  | "starfriend.es"  | "https://www.starfrie/"  |
+      | urls                | palabra   | urlrevisar             |
+      | "https://google.es" | "lowi.es" | "https://www.lowi.es/" |
--- features/steps/tutorialSteps.py
+++ features/steps/tutorialSteps.py
@@ -8,51 +8,76 @@ selenium: Selenium
 
 
 @Given('Encender el navegador')
+@When('Encender el navegador')
+@Then('Encender el navegador')
 def startBrower(context):
     global selenium
     selenium = context.type_driver
 
 
+@Given('El navegador introduce la URL "{web}"')
 @When('El navegador introduce la URL "{web}"')
+@Then('El navegador introduce la URL "{web}"')
 def step_impl(context, web):
     selenium.utilsDriver.browser_go_to(web)
 
 
+@Given('El navegador espera "{seconds}" segundos')
+@When('El navegador espera "{seconds}" segundos')
+@Then('El navegador espera "{seconds}" segundos')
+def step_impl(context, seconds):
+    selenium.utilsDriver.sleep(int(seconds))
+
+
+@Given('Se visualiza la pagina de busqueda de google')
 @When('Se visualiza la pagina de busqueda de google')
+@Then('Se visualiza la pagina de busqueda de google')
 def step_impl(context):
     selenium.utilsDriver.sleep(1)
 
 
+@Given('Buscar en google por "{web}"')
 @When('Buscar en google por "{web}"')
+@Then('Buscar en google por "{web}"')
 def step_impl(context, web):
     google = GooglePageObject(selenium)
     google.barra.send_keys(web)
     google.barra.submit()
 
 
+@Given('se abre una nueva pestana')
 @When('se abre una nueva pestana')
+@Then('se abre una nueva pestana')
 def step_impl(context):
     selenium.utilsDriver.open_new_tab()
 
 
+@Given('se selecciona la "{indice}" pestana')
 @When('se selecciona la "{indice}" pestana')
+@Then('se selecciona la "{indice}" pestana')
 def step_impl(context, indice):
     selenium.utilsDriver.change_tab(int(indice))
 
 
+@Given('se cierra la pestana "{indice}"')
 @When('se cierra la pestana "{indice}"')
+@Then('se cierra la pestana "{indice}"')
 def step_impl(context, indice):
     selenium.utilsDriver.change_tab(int(indice))
     selenium.utilsDriver.close()
 
 
+@Given('Seleccionar el primer resultado en google')
 @When('Seleccionar el primer resultado en google')
+@Then('Seleccionar el primer resultado en google')
 def step_impl(context):
     selenium.utilsDriver.sleep(2)
     result = google_result_search(selenium)
     result.resultFirst.click()
 
 
+@Given('Comprobar que lleva a "{url}"')
+@When('Comprobar que lleva a "{url}"')
 @Then('Comprobar que lleva a "{url}"')
 def step_impl(context, url):
     selenium.utilsDriver.sleep(2)
