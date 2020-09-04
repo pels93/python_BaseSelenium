175663932cc567973e844ab098ecf89c3b17d18c
pels93 <daniel-g-j@hotmail.com>
2020-09-04 18:34:30 +0200

    fixed step print events

--- features/steps/get_events_steps.py
+++ features/steps/get_events_steps.py
@@ -2,6 +2,7 @@ import time
 
 from behave import Given, When, Then
 from driver.typeDriver.selenium.selenium import *
+from driver.typeDriver.interfaces.dictionary_driver import *
 
 
 @Given('imprimir logs consola')
@@ -10,8 +11,9 @@ from driver.typeDriver.selenium.selenium import *
 def start_brower(context):
     time.sleep(3)
     selenium: Selenium = context.type_driver
-    driver: WebDriver = selenium.driver
-    Utils.print_info("Error force for print log browser")
-    for entry in driver.get_log('browser'):
-        print(entry)
-    assert 1 == 0
+    if selenium.browser == chrome:
+        driver: WebDriver = selenium.driver
+        Utils.print_info("Error force for print log browser")
+        for entry in driver.get_log('browser'):
+            print(entry)
+        assert 1 == 0
