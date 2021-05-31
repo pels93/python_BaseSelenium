0995b3e5faedba38a396dc37cae64ff40dba9eff
danigonz <daniel-g-j@hotmail.com>
2021-05-31 13:58:38 +0200

    update view equal mobile in chrome

--- driver/typeDriver/selenium/utilsSelenium/wrapperDrivers/chrome.py
+++ driver/typeDriver/selenium/utilsSelenium/wrapperDrivers/chrome.py
@@ -8,7 +8,7 @@ class Chrome():
 
     def start(self) -> WebDriver:
         return webdriver.Chrome(executable_path=ChromeDriverManager().install(),
-                                desired_capabilities=self._set_capabilities_())
+                                desired_capabilities=self._set_capabilities_(), options=self._set_options_())
 
     def _set_capabilities_(self):
         capabilities = DesiredCapabilities.CHROME.copy()
@@ -21,3 +21,9 @@ class Chrome():
         capabilities['webdriver_accept_untrusted_certs'] = True
         capabilities['webdriver_assume_untrusted_issuer'] = True
         return capabilities
+
+    def _set_options_(self):
+        mobile_emulation = {"deviceName": "Nexus 10"}
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+        # return chrome_options
